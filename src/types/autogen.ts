a47495e57335e4eaa9f9085901e235afc0635fae
apav-dev <72618502+apav-dev@users.noreply.github.com>
2023-09-11 11:33:16 -0400

    Initial commit

---
+++ src/types/autogen.ts
@@ -0,0 +1,36 @@
+export interface Address {
+	line1?: string,
+	line2?: string,
+	line3?: string,
+	sublocality?: string,
+	city?: string,
+	region?: string,
+	postalCode?: string,
+	extraDescription?: string,
+	countryCode?: string,
+}
+
+export interface ImageThumbnail {
+	url: string,
+	width: number,
+	height: number,
+}
+
+export interface Image {
+	url: string,
+	width: number,
+	height: number,
+	thumbnails?: ImageThumbnail[],
+	alternateText?: string,
+}
+
+export default interface FinPro {
+	name: string,
+	slug: string,
+	address: Address,
+	emails: string[],
+	mainPhone: any,
+	description: string,
+	headshot: Image,
+	c_jobTitle: string,
+}
